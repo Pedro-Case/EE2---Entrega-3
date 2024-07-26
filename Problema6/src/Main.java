94ce0194b054795104392fffac50e9ae197fbd62
Pedro-Case <137705805+Pedro-Case@users.noreply.github.com>
2024-07-26 17:14:49 -0300

    Comentários

--- Problema6/src/Main.java
+++ Problema6/src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        boolean genero;
-        for (int i = 0; i < 100; i++){
-            genero = i % 2 == 0;
-            Pessoa pessoa = new Pessoa(i, genero);
-            pessoa.start();
+        for (int i = 0; i < 100; i++){ // 100 Pessoas (50 homens e 50 mulheres) desejam usar o banheiro
+            Pessoa pessoa = new Pessoa(i, i % 2 == 0); // Criando cada thread. Se o número da thread for par, a pessao é homem, se for impar é mulher.
+            pessoa.start(); // Incializado as threads
         }
     }
 }
--- Problema6/src/Pessoa.java
+++ Problema6/src/Pessoa.java
@@ -3,32 +3,32 @@ import java.util.concurrent.Semaphore;
 
 public class Pessoa extends Thread {
     static ReentrantLock entrado_saindo = new ReentrantLock(); // Usado para indicar que uma nova pessoa acabou de chegar e quer usar o banheiro
-    static Semaphore banheiro = new Semaphore(3); // Usado para indicar o número de pessoas que podem estar no banheiro ao mesmo tempo
+    static Semaphore banheiro = new Semaphore(3); // Usado para indicar o número de pessoas que podem estar no banheiro simultânea
 
-    static boolean banheiro_feminino = false; // Impede que homens entrem no banheiro
-    static boolean banheiro_masculino = false; // Impede que mulheres entrem no banheiro
+    static boolean banheiro_feminino = false; // Essa variável booleana vai ficar verdadeira se em qualquer momento houver uma mulher no banheiro, então nenhum homem poderá entrar no banheiro
+    static boolean banheiro_masculino = false; // Essa variável booleana vai ficar verdadeira se em qualquer momento houver um homem no banheiro, então nenhuma mulher poderá entrar no banheiro
 
-    int nome;
+    int nome; // Um número inteiro que identifica a thread
     boolean homem; // Define qual será o gênero da pessoa que chegou. Valor de true indica que é um homem, false indica que é uma mulher
-    boolean usou_banheiro;
+    boolean usou_banheiro; // Essa variável fica falsa até que a pessoa consiga entrar e usar o banheiro, usamos ela para fazer um laço de repetição em que cada pessoa tenta entrar no banheiro assim que for possível
     public Pessoa(int nome, boolean homem) {
         this.nome = nome;
         this.homem = homem;
         this.usou_banheiro = false;
     }
-    private void entrando_banheiro() throws InterruptedException {
-        entrado_saindo.lock();
+    private void entrando_banheiro() throws InterruptedException { //Essa função representa a pessao tentando entrar no banheiro
+        entrado_saindo.lock(); // Essa trava permite realizar todas as alterações referentes a uma pessoa entrar no banheiro, como ver quantas cabines estão disponíveis e se for o primeiro a entrar, definir se o banheiro se tornou exclusivo para homens ou mulheres
         int disponivel = banheiro.availablePermits();
-        if (disponivel > 0 && ((homem && banheiro_masculino) || (!homem && banheiro_feminino))) {
+        if (disponivel > 0 && ((homem && banheiro_masculino) || (!homem && banheiro_feminino))) { // Se houver alguma cabine disponível e a pessoa for de um gênero permitido ela consegue entrar no banheiro
             if (homem){
                 System.out.println(nome + " (Homem) entrou no banheiro");
             } else {
                 System.out.println(nome + " (Mulher) entrou no banheiro");
             }
-            banheiro.acquire(1);
+            banheiro.acquire(1); // Ocupa uma cabine do banheiro
             entrado_saindo.unlock();
             saindo_banheiro();
-        } else if (!banheiro_feminino && !banheiro_masculino){
+        } else if (!banheiro_feminino && !banheiro_masculino){ // Se o banheiro não tiver nenhum gênero definido, significa que não tem ninguém no banheiro, então a pessoa consegue entrar, então deve deixar o banheiro exclusivo para um gênero
             if (homem){
                 System.out.println(nome + " (Homem) entrou no banheiro, e agora nenhuma mulher poderá entrar");
                 banheiro_masculino = true;
@@ -36,25 +36,25 @@ public class Pessoa extends Thread {
                 System.out.println(nome + " (Mulher) entrou no banheiro, e agora nenhum homem poderá entrar");
                 banheiro_feminino = true;
             }
-            banheiro.acquire(1);
+            banheiro.acquire(1); // Ocupa uma cabine do banheiro
             entrado_saindo.unlock();
             saindo_banheiro();
         } else {
-            entrado_saindo.unlock();
+            entrado_saindo.unlock(); // Não consegue entrar, então permite que outra pessoa consiga tentar entrar
         }
 
     }
-    private void saindo_banheiro() throws InterruptedException {
-         sleep(((nome % 3) + 1));
-         entrado_saindo.lock();
+    private void saindo_banheiro() throws InterruptedException { // Essa função representa a pessoa usar o banheiro e sair dele
+         sleep(((nome % 3) + 1)); // Representa o tempo em que a pessoa passa para usar o banheiro (deixei o tempo de sleep baixo para permitir que aconteça casos mais interessantes)
+         entrado_saindo.lock(); // Depois do tempo, a pessoa consegue sair do banheiro
          usou_banheiro = true;
         if (homem){
             System.out.println(nome + " (Homem) saiu do banheiro");
         } else {
             System.out.println(nome + " (Mulher) saiu do banheiro");
         }
-         banheiro.release(1);
-         if (banheiro.availablePermits() == 3) {
+         banheiro.release(1); // Libera uma cabine
+         if (banheiro.availablePermits() == 3) { // Se o número de cabines disponíveis do banheiro for 3 após essa pessoa sair, significa que o banheiro está vazio, então não deve mais ser exclusivo a nenhum gênero
              if (homem){
                  System.out.println(nome + " deixou o banheiro vazio, agora qualquer um pode entrar");
                  banheiro_masculino = false;
@@ -63,15 +63,15 @@ public class Pessoa extends Thread {
                  banheiro_feminino = false;
              }
          }
-         entrado_saindo.unlock();
+         entrado_saindo.unlock(); // A pessoa termina de sair
     }
     public void run() {
         if (homem) {
             System.out.println(nome + " (Homem) deseja usar o banheiro");
         } else {
-            System.out.println(nome + " (Homem) deseja usar o banheiro");
+            System.out.println(nome + " (Mulher) deseja usar o banheiro");
         }
-        while (!usou_banheiro) {
+        while (!usou_banheiro) { // Enquanto ainda não usou o banheiro, a pessoa constantemente vai tentar entrar no banheiro para usá-lo
             try {
                 entrando_banheiro();
             } catch (InterruptedException e) {
