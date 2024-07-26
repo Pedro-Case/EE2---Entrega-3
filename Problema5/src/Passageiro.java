7349ee2f7457c9aac154c355392909e599ac8ec2
Marcus Vinícius <122657414+MarkVinne@users.noreply.github.com>
2024-07-26 18:38:40 -0300

    Adicionando comentarios

--- Problema5/src/Passageiro.java
+++ Problema5/src/Passageiro.java
@@ -6,21 +6,22 @@ public class Passageiro extends Thread{
         this.onibus = onibus;
     }
     public void run(){
-        System.out.println("Passageiro " + nome + " anda até a parada de onibus");
-        onibus.onibus_passageiro_chegando.lock();
+        System.out.println("Passageiro " + nome + " anda até a parada de onibus");// mostra que a thread foi inicializada
+        // se o onibus ja estiver na parada, o lock impedira a execução deste bloco, e a mensagem "espera na parada" nao sera impressa
+        onibus.onibus_passageiro_chegando.lock(); // lock para sincronizar a chegada dos passageiros
         System.out.println("Passageiro " + nome + " espera na parada para subir no onibus");
         onibus.passageiros_na_parada++;
         onibus.onibus_passageiro_chegando.unlock();
         boolean embarcou = false;
-        while (!embarcou) {
-            onibus.passageiro_embarcando.lock();
-            embarcou = onibus.capacidade_onibus.tryAcquire();
+        while (!embarcou) { // loop até que o passageiro consiga embarcar
+            onibus.passageiro_embarcando.lock(); // lock para sincronizar o embarque dos passageiros
+            embarcou = onibus.capacidade_onibus.tryAcquire(); // tenta adquirir um assento no ônibus
             if (embarcou) {
                 System.out.println("Passageiro " + nome + " subiu no onibus. Assentos restantes: " + onibus.capacidade_onibus.availablePermits());
                 onibus.passageiros_na_parada--;
                 onibus.passageiros_transportados++;
             }
-            onibus.passageiro_embarcando.unlock();
+            onibus.passageiro_embarcando.unlock();// libera o lock para os outros passageiros
         }
     }
 }
